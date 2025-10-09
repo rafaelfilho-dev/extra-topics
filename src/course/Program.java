06496244f11fd97784ff4a6a3f3577c0620744ba
Rafael Filho <rafael.antoniofilho@hotmail.com>
2025-10-09 17:51:03 -0300

    Minha solução para o problema do GPT

--- src/course/Program.java
+++ src/course/Program.java
@@ -3,37 +3,45 @@ package course;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int totalQuantity;
+        System.out.println("Quantos produtos você comprou?");
+        totalQuantity = sc.nextInt();
 
-        int higher = max(a, b, c);
+        double higherPrice, averagePrice, totalPrice;
+        String expensiverProduct = "Nenhum produto foi comprado";
+        higherPrice = 0;
+        totalPrice = 0;
 
-        showResult(higher);
+        for(int i = 0; i<totalQuantity; i++){
+            String productName;
+            double productPrice;
+            int quantity;
+            System.out.println("Qual o nome do produto?");
+            productName = sc.next();
+            System.out.println("Qual o preço unitário do produto?");
+            productPrice = sc.nextDouble();
+            System.out.println("Quantos produtos você comprou?");
+            quantity = sc.nextInt();
+
+            totalPrice += productPrice * quantity;
+
+            if(productPrice > higherPrice){
+                higherPrice = productPrice;
+                expensiverProduct = productName;
+            }
+        }
+
+        averagePrice = totalPrice / totalQuantity;
+
+        System.out.printf("Total da compra: R$%.2f%n", totalPrice);
+        System.out.printf("Média por produto: R$%.2f%n", averagePrice);
+        System.out.printf("Produto mais caro: %s (R$%.2f)%n", expensiverProduct, higherPrice);
+
 
         sc.close();
-    }
-    public static int max(int x, int y, int z){
-        int aux;
-
-        if (x > y && x > z){
-            aux = x;
-        }
-        else if (y > z){
-            aux = y;
-        }
-        else {
-            aux = z;
-        }
-
-        return aux;
-    }
-
-    public static void showResult(int value){
-        System.out.println("Higher: " + value);
     }
 }
